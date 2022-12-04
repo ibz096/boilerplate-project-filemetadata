d471e384e257655a2ed93522367ed243357b21df
Ibrahim Ahmed <13389823+ibz096@users.noreply.github.com>
2022-12-04 21:34:31 +0000

    Implemented required endpoint

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer')
+const upload = multer({dest: 'uploads/'})
 require('dotenv').config()
 
 var app = express();
@@ -7,14 +9,21 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), async (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
 
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
 });
